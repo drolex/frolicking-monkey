3be1c6116b33d46d1397920b54530f65bc33018a
Alexandre Bourdeu <abourdeu@yahoo.fr>
2014-07-22 15:14:36 +0100

    add build subfolders

---
+++ build-OSGridConverter-Android_for_armeabi_v7a_GCC_4_8_Qt_5_3_0-Release/android-build/gen/org/kde/necessitas/ministro/IMinistroCallback.java.d
@@ -0,0 +1,3 @@
+C:\Users\alexandre.bourdeu\Documents\GitHub\frolicking-monkey\build-OSGridConverter-Android_for_armeabi_v7a_GCC_4_8_Qt_5_3_0-Release\android-build\gen\org\kde\necessitas\ministro\IMinistroCallback.java: \
+  C:\Users\alexandre.bourdeu\Documents\GitHub\frolicking-monkey\build-OSGridConverter-Android_for_armeabi_v7a_GCC_4_8_Qt_5_3_0-Release\android-build\src\org\kde\necessitas\ministro\IMinistroCallback.aidl 
+
